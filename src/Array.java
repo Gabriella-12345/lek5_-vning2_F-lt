fc5cc7626d453dfeebf1f4cc07f6f8fbd9f6dee2
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-09-27 10:42:17 +0200

    first commit

---
+++ src/Array.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        int[] fält1 = new int[4];
+
+        fält1[0] = a;
+        fält1[1] = b;
+        fält1[2] = c;
+        fält1[3] = d;
+    }
+}
